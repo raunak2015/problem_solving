2d1a7460a61c2669764c4db14b2fb12d8b1b38b7
Raunak shahu <raunakshahu.cg@gmail.com>
2026-01-02 09:23:00 +0530

    change in Q74

--- level_8/74.js
+++ level_8/74.js
@@ -5,24 +5,27 @@
 // Input: [1, 2] and [3, 4] → Output: []
 // Input: [1, 1, 2] and [1, 2, 2] → Output: [1, 2]
 
-let arr = [1, 2, 3, 4];
+let arr1 = [1, 2, 3, 4];
 let arr2 = [1, 2, 2];
 
 let res = [];
 let i = 0;
 let j = 0;
 
-while (i < arr.length && j < arr2.length) {
-    if (arr[i] == arr2[i]) {
-        res.push(arr[i]);
-        i++;
-        j++;
+while (i < arr1.length && j < arr2.length) {
+  if (arr1[i] === arr2[j]) {
+    if (!res.includes(arr1[i])) {   
+      res.push(arr1[i]);
     }
-    else if (arr[i] > arr2[j]) {
-        i++;
-    }
-    else {
-        j++
-    }
+    i++;
+    j++;
+  } 
+  else if (arr1[i] < arr2[j]) {
+    i++;
+  } 
+  else {
+    j++;
+  }
 }
-console.log(res)
+
+console.log(res); 
